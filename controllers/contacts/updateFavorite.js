5535e83f426337523f52b11c50fded99ecc06979
Serhii Kaushnian <skaushnjan@gmail.com>
2022-12-15 22:15:38 +0100

    Update updateFavorite.js

--- controllers/contacts/updateFavorite.js
+++ controllers/contacts/updateFavorite.js
@@ -3,6 +3,7 @@ const { Contact } = require('../../models/contact');
 
 const updateFavorite = async (req, res, next) => {
   const { id } = req.params;
+
   const updatedContact = await Contact.findByIdAndUpdate(id, req.body, { new: true });
 
   if (!updatedContact) {
