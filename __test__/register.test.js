ba8f12890d6261266226093eac3c4330f87300da
Serhii Kaushnian <skaushnjan@gmail.com>
2023-01-10 15:12:57 +0100

    test try

--- __test__/register.test.js
+++ __test__/register.test.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const request = require('supertest');
-const app = require('../routes/api/auth');
+const app = require('../app');
 
 require('dotenv').config();
 /* Connecting to the database before each test. */
@@ -12,13 +12,15 @@ beforeEach(async () => {
 afterEach(async () => {
   await mongoose.connection.close();
 });
-
+const mockData = {
+  email: '1testEmail@gmail.com',
+  password: '123456',
+};
 describe('POST /api/register', () => {
   it('should create a user', async () => {
-    const res = await request(app).post('/api/register').send({
-      email: 'testEmail@gmail.com',
-      password: '123456',
-    });
+    const res = await request(app).post('/api/auth/register').send(mockData);
     expect(res.statusCode).toBe(201);
+    expect(res.body.email).toBe(mockData.email);
+    expect(res.body.subscription).toBe('starter');
   });
 });
